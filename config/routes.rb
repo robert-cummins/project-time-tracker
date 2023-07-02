94a7577a8c4978a223f75ade9270aa58256cd769
robert-cummins <robert.k.cummins@gmail.com>
2023-07-02 17:01:03 +1200

    Set up devise backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   
- 
+  get '/current_user', to: 'current_user#index'
   root 'homepage#index'
   
 end
